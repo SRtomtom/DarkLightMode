f8bcc70525de91ed5d3646af583af4f64a180470
Washington Rodrigues <Washingtonrodrigues001@gmail.com>
2023-05-15 12:56:42 -0300

    Local Storage Dark Mode

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const chk = document.getElementById("chk");
-//"const chk" serve para criar a variavel e o depois do igual é o que a variavel vai fazer
+const chk = document.getElementById("chk"); //"const chk" serve para criar a variavel e o depois do igual é o que a variavel vai fazer
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
+
+//carrega modo dark / light
+function locadTheme() {
+  const darkMode = localStorage.getItem("dark");
+
+  if (darkMode) {
+    toggleDarkMode();
+  }
+}
+
+locadTheme();
 
 chk.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
+  toggleDarkMode();
+
+  //salva e/ou remove modo dark
+  localStorage.removeItem("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1); //a flag'1' é possivel ver em inspecionar > application > local Storage.
+  }
 });
